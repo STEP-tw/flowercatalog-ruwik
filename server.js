c7603ff2e978505014a161267d6569971674320e
ruwik <manindrakrishna@gmail.com>
2018-01-09 18:09:03 +0530

    now there is a way to check user

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ let fs = require('fs');
 let http = require('http');
 let webApp = require('./webApp.js');
 
+
 const giveFileType = function(url){
   return url.slice(url.lastIndexOf('.'));
 }
@@ -35,16 +36,58 @@ const readAndWriteFile = function(filePath,res){
   });
 };
 
+const registered_users = [{userName:'manikm',name:'Manindra Krishna Motukuri'}]
+
+let loadUser = (req,res)=>{
+  let sessionid = req.cookies.sessionid;
+  let user = registered_users.find(u=>u.sessionid==sessionid);
+  if(sessionid && user){
+    req.user = user;
+  }
+};
+let redirectLoggedInUserToHome = (req,res)=>{
+  if(req.urlIsOneOf(['/public/html/login.html','/public/html/guestBook.html']) && req.user) res.redirect('/public/html/writeCommentHere.html');
+}
+let redirectLoggedOutUserToLogin = (req,res)=>{
+  if(req.urlIsOneOf(['/logout']) && !req.user) res.redirect('/public/html/login.html');
+}
 
 const app = webApp.create();
+app.use(loadUser);
+app.use(redirectLoggedInUserToHome);
+app.use(redirectLoggedOutUserToLogin);
 app.get('/',(req,res)=>{
   res.redirect('/index.html');
   return;
 });
 
+app.get('/logout',(req,res)=>{
+  res.setHeader('Set-Cookie',[`loginFailed=false,Expires=${new Date(1).toUTCString()}`,`sessionid=0,Expires=${new Date(1).toUTCString()}`]);
+  delete req.user.sessionid;
+  res.redirect('/public/html/login.html');
+});
+
 app.get('/index.html',(req,res)=>{
   readAndWriteFile('./index.html',res);
 })
+app.get('/login',(req,res)=>{
+  res.setHeader('Content-type','text/html');
+  if(req.cookies.logInFailed) res.write('<p>logIn Failed</p>');
+  res.write('<form method="POST"> <input name="userName"><input type="submit"></form>');
+  res.end();
+});
+app.post('/login',(req,res)=>{
+  let user = registered_users.find(u=>u.userName==req.body.userName);
+  if(!user) {
+    res.setHeader('Set-Cookie',`logInFailed=true`);
+    res.redirect('/login');
+    return;
+  }
+  let sessionid = new Date().getTime();
+  res.setHeader('Set-Cookie',`sessionid=${sessionid}`);
+  user.sessionid = sessionid;
+  res.redirect('/public/html/writeCommentHere.html');
+});
 
 app.get('/public/css/index.css',(req,res)=>{
   readAndWriteFile('.'+req.url,res);
@@ -106,11 +149,7 @@ app.get('/public/html/login.html',(req,res)=>{
   readAndWriteFile('.'+req.url,res);
 })
 
-app.post('/public/html/writeCommentHere.html',(req,res)=>{
-  readAndWriteFile('.'+req.url,res);
-})
-
-app.post('/public/html/guestBook.html',(req,res)=>{
+app.post('/updateComments',(req,res)=>{
   res.redirect('/public/html/writeCommentHere.html');
   return;
 })
@@ -118,7 +157,6 @@ app.post('/public/html/guestBook.html',(req,res)=>{
 app.get('/public/html/writeCommentHere.html',(req,res)=>{
   readAndWriteFile('.'+req.url,res);
 })
-
 
 app.get('/data/data.js',(req,res)=>{
   readAndWriteFile('.'+req.url,res);
